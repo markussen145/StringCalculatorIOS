a1c7797c845e1064348ca442765507dad80eff33
Pedro Vicente Gómez Sánchez <pedro@karumi.com>
2019-12-11 11:36:07 +0100

    Initial Commit

---
+++ StringCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StringCalculator
+//
+//  Created by Pedro Gómez on 11/12/2019.
+//  Copyright © 2019 Pedro Gómez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
